8761a5fdb6b4cfa9da06f3f58904eb143411859e
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-07-18 23:38:47 +1000

    Feast: dropdown menu

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from 'el-transition'
+
+export default class extends Controller {
+  static targets = ['openUserMenu']
+  connect() {
+    // this.element.textContent = "Hello World!"
+    this.openUserMenuTarget.addEventListener('click', toggleDropdownMenu)
+
+    function toggleDropdownMenu() {
+      toggle(document.getElementById('menu-dropdown-items'))
+    }
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
