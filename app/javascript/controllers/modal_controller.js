e2f43db9b2f0c9e78450e77e2b8fe553bb5c2a91
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-07-19 19:39:30 +1000

    Feast:modal controller, modal shows with a click event

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,6 +4,20 @@ import { enter, leave, toggle } from 'el-transition'
 export default class extends Controller {
   
   connect() {
-    console.log("Modal Controller connected")
+    enter(document.getElementById('modal-wrapper'))
+    enter(document.getElementById('modal-backdrop'))
+    enter(document.getElementById('modal-panel'))
+
+    document.getElementById('modal-wrapper').addEventListener('click',closeModal)
+
+    function closeModal(event){
+        const modalClicked = document.getElementById('modal-panel').contains(event.target)
+        if(!modalClicked){
+            leave(document.getElementById('modal-wrapper'))
+            leave(document.getElementById('modal-backdrop'))
+            leave(document.getElementById('modal-panel'))
+            document.getElementById('modal-wrapper').removeEventListener('click',closeModal)
+        }
+    }
   }
 }
