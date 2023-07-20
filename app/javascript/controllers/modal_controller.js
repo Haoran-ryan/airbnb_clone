262f14a35fde74e6083e0bc4339a50975f99cbb6
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-07-20 10:25:08 +1000

    Feast: modal pops up when login or signup is clicked

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,20 +4,23 @@ import { enter, leave, toggle } from 'el-transition'
 export default class extends Controller {
   
   connect() {
+    document.getElementById('modal-wrapper').addEventListener('click',this.closeModal)
+  };
+
+  closeModal(event){
+      const modalClicked = document.getElementById('modal-panel').contains(event.target)
+      if(!modalClicked && event.target.id !== 'modal-trigger'){
+          leave(document.getElementById('modal-wrapper'))
+          leave(document.getElementById('modal-backdrop'))
+          leave(document.getElementById('modal-panel'))
+          document.getElementById('modal-wrapper').removeEventListener('click',this.closeModal)
+      }
+  };
+  
+
+  showModal() {
     enter(document.getElementById('modal-wrapper'))
     enter(document.getElementById('modal-backdrop'))
     enter(document.getElementById('modal-panel'))
-
-    document.getElementById('modal-wrapper').addEventListener('click',closeModal)
-
-    function closeModal(event){
-        const modalClicked = document.getElementById('modal-panel').contains(event.target)
-        if(!modalClicked){
-            leave(document.getElementById('modal-wrapper'))
-            leave(document.getElementById('modal-backdrop'))
-            leave(document.getElementById('modal-panel'))
-            document.getElementById('modal-wrapper').removeEventListener('click',closeModal)
-        }
-    }
-  }
+  };
 }
